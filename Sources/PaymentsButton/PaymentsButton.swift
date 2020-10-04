bff5c7e1e1cc894ea4c1be06ed46980b4818a746
benjaminsage <benjaminlsage@gmail.com>
2020-10-04 15:58:09 -0700

    Added initial round of documentation comments

--- Sources/PaymentsButton/PaymentsButton.swift
+++ Sources/PaymentsButton/PaymentsButton.swift
@@ -3,9 +3,17 @@ import PassKit
 import SwiftUI
 
 @available(iOS 13.0, *)
+/// A payment button wrapper view around Apple's PassKit `PKPaymentButton`
+/// which allows the button to be drawn and rendered complely using SwiftUI
 public struct PaymentButton: View {
+    
+    /// The action to be performed when the user taps the button
     var action: () -> Void
+    
+    /// The text written on the button
     var type: PKPaymentButtonType = .buy
+    
+    /// The color that the button renders
     var style: PKPaymentButtonStyle = .black
     
     public var body: some View {
